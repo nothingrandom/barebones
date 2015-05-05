7198a92b8ae5b8c0ab01f98acfddd4efcaeb8b78
Benjamin Hollway <benjamin@nothingrandom.com>
2015-05-05 12:47:03 +0100

    Basic html structure

---
+++ src/inc/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
+<!--[if IE 10 ]>    <html lang="en" class="ie10"> <![endif]-->
+<!--[if (gt IE 10)|!(IE)]><!--> <html lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1.0,minimum-scale=1.0"/>
+		<meta name="msapplication-tap-highlight" content="no" />
+
+		<title>barebones.io</title>
+		<meta name="keywords" content=""/>
+		<meta name="description" content="">
+
+		<link rel="icon" type="image/png" href="assets/images/"/>
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+	</head>
+	<body>
