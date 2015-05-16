e692458dee0b6cd78c00a473417dd5a8a35d5061
Benjamin Hollway <benjamin@nothingrandom.com>
2015-05-16 18:31:06 +0100

    Add config for paths

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,27 @@ var del = require('del'),
 
 var plumber = require('gulp-plumber'),
 	sourcemaps = require('gulp-sourcemaps');
+
+// Config
+// ==============================
+var config = {
+	path: {
+		root: path.resolve(__dirname) + '/'
+	}
+};
+
+// Base
+config.path.doc = config.path.root + 'src/';
+config.path.assets = config.path.doc + 'assets/';
+config.path.build = config.path.root + 'build/';
+
+// Source
+config.path.sass = config.path.assets + 'scss/';
+config.path.js_src = config.path.assets + 'js_src/';
+config.path.images = config.path.assets + 'images_src/';
+config.path.fonts = config.path.assets + 'fonts/';
+
+// Compiled
+config.path.css = config.path.assets + 'css/';
+config.path.js = config.path.assets + 'js/';
+config.path.img = config.path.assets + 'images/';
